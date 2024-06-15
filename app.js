17fdc83e3c6578e7b85f9f514c61d70ac1600af2
sagar-developer08 <08sagar.s@gmail.com>
2024-06-15 17:02:01 +0530

    change in db

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const express = require('express');
 const sequelize = require('./config/databaseConfig');
 const authRoutes = require('./routes/authRoutes');
 // const AWS = require('./config/awsConfig');
-
+const cors = require('cors')
 const app = express();
 
 app.use(express.json());
-
+app.use(cors())
 // const { createSession, deleteSession, getSessionByUserId } = require('./sessionManager');
 
 
